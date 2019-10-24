fb2beeb124c8868d73729286e6785bb1851c616d
arturmarszycki <artur.marszycki@gmail.com>
2019-10-24 22:26:22 +0200

    Updates

---
+++ precache-manifest.b252d502ce1447b136bf42d8102874d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b5f0cfc7b274a432d032e380e6073c",
+    "url": "/to_do_app/index.html"
+  },
+  {
+    "revision": "066fb6eb8afa604608a3",
+    "url": "/to_do_app/static/css/main.fd497182.chunk.css"
+  },
+  {
+    "revision": "00c6e45f4fbed101a619",
+    "url": "/to_do_app/static/js/2.fe43e55c.chunk.js"
+  },
+  {
+    "revision": "066fb6eb8afa604608a3",
+    "url": "/to_do_app/static/js/main.1cd2fb7d.chunk.js"
+  },
+  {
+    "revision": "c6cb630366d337ab4863",
+    "url": "/to_do_app/static/js/runtime-main.21a53536.js"
+  }
+]);
